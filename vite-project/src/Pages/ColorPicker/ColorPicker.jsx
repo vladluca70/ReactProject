2bdd7b81884b4e3685c0dc082be5e485a1a2681b
vladluca70 <vladluca70@gmail.com>
2025-07-30 15:19:18 +0300

    color picker- finished

--- vite-project/src/Pages/ColorPicker/ColorPicker.jsx
+++ vite-project/src/Pages/ColorPicker/ColorPicker.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./ColorPicker.css";
 
+function ColorPicker() {
+  const [color, setColor] = useState("#FFFFFF");
 
-function ColorPicker(){
+  const handleColorChange = (e) => {
+    setColor(e.target.value);
+  };
 
-    const [color, setColor]=useState("#FFFFFF");
+  return (
+    <main className="color-picker-container">
+      <section className="color-picker-section">
+        <input
+          type="color"
+          value={color}
+          onChange={handleColorChange}
+          aria-label="Choose color"
+          className="color-input"
+        />
+        <p>
+          The code is <span className="color-code">{color}</span>
+        </p>
+      </section>
 
-    return(
-        <>
-            <div>
-                <input type="color" 
-                    value={color}
-                />
-            </div>
-
-            <br/>
-            <Link to={'/'}>Go back home</Link>
-        </>
-    );
+      <Link to="/">Go back home</Link>
+    </main>
+  );
 }
 
 export default ColorPicker;
